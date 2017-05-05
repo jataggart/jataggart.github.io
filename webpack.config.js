64862979d1e360dd1d35f6954bf4ba4a7f585104
Jay Taggart <jataggart@gmail.com>
2017-05-04 23:26:28 -0500

    no need for plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
